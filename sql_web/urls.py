10c4c32576aab6c7684a176fbec90780c2a07e18
Ernir <e.ernir@gmail.com>
2016-02-11 22:56:39 +0000

    Make the section view class-based

--- sql_web/urls.py
+++ sql_web/urls.py
@@ -3,6 +3,6 @@ from sql_web import views
 
 urlpatterns = [
     url(r"^vidfangsefni/$", views.sections, name="sections"),
-    url(r"^vidfangsefni/(?P<section_slug>.+)/$", views.section,
-        name="section"),
+    url(r"^vidfangsefni/(?P<section_slug>.+)/$",
+        views.SectionView.as_view(), name="section"),
 ]
--- sql_web/views.py
+++ sql_web/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import View
 from sql_web.models import Section
 
 
-def section(request, section_slug):
+class BaseView(View):
+    """
+    An "abstract view" to manage the elements all of the site's
+    pages have in common
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.params = {
+            "title": "SQL kennslusíða",  # Default value
+        }
+
+
+class SectionView(BaseView):
     """
     Defines the page of a single lexical section.
     Finds the section based on its slugified title,
     passed in by section_slug.
     """
 
-    the_section = get_object_or_404(Section, slug=section_slug)
-    return render(request, "section.html", {"section": the_section})
+    def get(self, request, section_slug):
+        the_section = get_object_or_404(Section, slug=section_slug)
+        self.params["section"] = the_section
+        self.params["title"] = the_section.title
+        return render(request, "section.html", self.params)
 
 
 def sections(request):
@@ -23,4 +40,4 @@ def sections(request):
 
 
 def index(request):
-    pass # ToDo make one
+    pass  # ToDo make one
