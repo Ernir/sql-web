fe5214cb72c742701158ac630f504bc028e1dca9
Ernir <e.ernir@gmail.com>
2016-03-03 21:41:21 +0000

    Show text labels next to overview graph nodes

--- sql_web/static/d3-main-function.js
+++ sql_web/static/d3-main-function.js
@@ -1,19 +1,19 @@
 d3.xhr("/bakatil/vidfangsefni/", "application/json", function (request) {
     var data = JSON.parse(request.response);
-    draw(data.nodes, data.links);
+    drawGraph(data);
 });
 
-function draw(nodes, links) {
+var drawGraph = function (graph) {
 
     var modifiedLinks = [];
-    links.forEach(function (e) {
+    graph.links.forEach(function (e) {
         // Get the source and target nodes
-        var sourceNode = nodes.filter(
+        var sourceNode = graph.nodes.filter(
             function (n) {
                 return n.id === e.source;
             }
         )[0];
-        var targetNode = nodes.filter(
+        var targetNode = graph.nodes.filter(
             function (n) {
                 return n.id === e.target;
             }
@@ -26,20 +26,24 @@ function draw(nodes, links) {
         });
     });
 
-    var width = 400, height = 400;
+    var width = 900;
+    var height = 500;
 
     var color = d3.scale.category20();
 
     var force = d3.layout.force()
-        .nodes(nodes)
-        .links(links)
-        .charge(-120)
-        .linkDistance(30)
+        .charge(-200)
+        .linkDistance(300)
         .size([width, height]);
 
     var svg = d3.select("body").append("svg")
         .attr("width", width)
         .attr("height", height);
+
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
 
     var link = svg.selectAll(".link")
         .data(modifiedLinks)
@@ -49,52 +53,46 @@ function draw(nodes, links) {
             return Math.sqrt(d.value);
         });
 
-    var node = svg.selectAll(".node")
-        .data(nodes)
-        .enter().append("circle")
+    var gnodes = svg.selectAll('g.gnode')
+        .data(graph.nodes)
+        .enter()
+        .append('g')
+        .classed('gnode', true);
+
+    var node = gnodes.append("circle")
         .attr("class", "node")
-        .attr("r", 5)
+        .attr("r", 10)
         .style("fill", function (d) {
             return color(d.group);
         })
         .call(force.drag);
 
-    node.append("title")
+    var labels = gnodes.append("text")
         .text(function (d) {
             return d.name;
         });
 
-    var x = 0;
-    var tickLim = Infinity; // ToDo decide if this should be less than inf
     force.on("tick", function () {
-        if (x < tickLim) {
-            link.attr("x1", function (d) {
-                return d.source.x;
+        link.attr("x1", function (d) {
+            return d.source.x;
+        })
+            .attr("y1", function (d) {
+                return d.source.y;
             })
-                .attr("y1", function (d) {
-                    return d.source.y;
-                })
-                .attr("x2", function (d) {
-                    return d.target.x;
-                })
-                .attr("y2", function (d) {
-                    return d.target.y;
-                });
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
 
-            node.attr("cx", function (d) {
-                return d.x;
-            })
-                .attr("cy", function (d) {
-                    return d.y;
-                });
-            x++;
-        }
+        gnodes.attr("transform", function (d) {
+            return 'translate(' + [d.x, d.y] + ')';
+        });
     });
-
-    force.start();
 
     d3.selectAll(".node").on("click", function () {
         var selectedNode = d3.select(this);
         window.location = selectedNode[0][0].__data__.location;
     });
-}
+};
