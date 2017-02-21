f44a4832ca03173cbcbd4ee9e4b3c5b72e1ac998
ernir <e.ernir@gmail.com>
2017-02-21 14:56:12 +0000

    Refer to the Django settings file from the WSGI config file

--- SQLweb/wsgi.py
+++ SQLweb/wsgi.py
@@ -10,5 +10,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+from os import environ
+
+environ["DJANGO_SETTINGS_MODULE"]="SQLweb.settings"
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
